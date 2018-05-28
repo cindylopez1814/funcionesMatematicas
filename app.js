7553fdf3f7093ee0c465d3690fda4770c44fc61c
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-05-28 18:08:46 -0400

    subiendo ejercicio resuelto

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const add = 9824 + 777;
+window.onload = () => {
+  const btn = document.getElementById("btn");
+  const demo = document.getElementById("demo");
 
-const multiply = 36325 * add;
+  // Funcion suma
+  add = (n1,n2) => {
+    return n1 + n2
+  };
+  
+  //Funcion multiplicacion
+  multiply = (n3, n4) => {
+    return n3 * n4;
+  };
 
-console.log(multiply)
+  // Argumentos de la funcion
+  const func1 = add (9824,777);
+  const result = multiply (36325, func1);
+  
+  // Funcionalidad boton
+  btn.addEventListener("click", function(){
+    demo.innerHTML = result
+  });
+};
